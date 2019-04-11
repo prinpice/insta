cb2a21bbfba2d7e14da62bdbd12381d6492d1372
Kelly Jung <qkdrkd1994@gmail.com>
2019-04-11 06:35:47 +0000

    Post 모델에 image 속성 추가 & migrate 완료

--- posts/models.py
+++ posts/models.py
@@ -3,6 +3,8 @@ from django.db import models
 # Create your models here.
 class Post(models.Model):
     content = models.CharField(max_length=150)
+    image = models.ImageField(blank=True) # 업로드 된 파일이 들어오면 1. 특정위치에 넣고, 2. 주소값을 저장한다.
     
     def __str__(self):
         return self.content
+        
